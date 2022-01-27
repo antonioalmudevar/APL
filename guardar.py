781f2855c0d12a663d4747899b91ab67cab2f3e4
Antonio Almudévar <91321976+antonioalmudevar@users.noreply.github.com>
2022-01-27 11:59:45 +0100

    Delete guardar.py

--- guardar.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pickle
-from funciones import binary2matrix
-
-filename = sys.argv[1]
-Dt = (int)(sys.argv[2])
-B = binary2matrix(filename,Dt)
-with open('./data_pkl/'+filename+'_'+str(Dt)+'.pkl', 'wb') as f:
-    pickle.dump(B, f)
